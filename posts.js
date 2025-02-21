1f96bcaaa051ca0ba0f948fea84fe8378e0519fd
sakaaa-exe <sakharaditya799@gmail.com>
2025-02-21 20:18:14 +0700

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,50 @@
+// Sample API
+    const posts = [
+      {
+        id: 1,
+        username: "Alice",
+        profilePicture: "",
+        datetime: "2025-02-18 10:15",
+        role: "user",
+        fact: "Saya suka mengumpulkan batu kecil di taman.",
+        reason: "Setiap batu menyimpan kenangan.",
+        reactions: {
+          "🤨": 0,
+          "🍆": 0,
+          "👍": 2,
+          "😂": 1,
+          "❤️": 0
+        }
+      },
+      {
+        id: 2,
+        username: "Bob",
+        profilePicture: "bob.jpeg",
+        datetime: "2025-02-18 11:30",
+        role: "user",
+        fact: "gw suka femboy",
+        reason: "g tw, gw suka aja",
+        reactions: {
+          "🤨": 20,
+          "🍆": 2,
+          "👍": 0,
+          "😂": 0,
+          "❤️": 2
+        },
+        adminReply: {
+          username: "sakhrdty",
+          profilePicture: "admin.jpg",
+          datetime: "2025-02-18 12:00",
+          role: "admin",
+          reply: "wedew",
+          reactions: {
+            "🤨": 1,
+            "🍆": 7,
+            "👍": 0,
+            "😂": 0,
+            "❤️": 0
+          }
+        }
+      }
+    ];
+    
