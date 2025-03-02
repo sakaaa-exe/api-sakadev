028e51467a07bc0d3a7ba252e16d7f770698453e
sakaaa-exe <sakharaditya799@gmail.com>
2025-03-02 14:29:23 +0700

    Add files via upload

---
+++ datajurnal.js
@@ -0,0 +1,12 @@
+const tableData = [
+    {
+      day: 1,
+      sahur: "•",
+      sholat: { subuh: "•", dzuhur: "•", ashar: "•", maghrib: "", isya: "" },
+      tarawih: "",
+      puasa: "",
+      buka: "",
+      ngaji: ""
+    }
+    
+  ];
